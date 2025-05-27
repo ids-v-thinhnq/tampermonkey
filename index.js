5fd7bd47fcc924cd179aa05078a449f556c562e9
IDS-V_Thinhnq <75286403+ids-v-thinhnq@users.noreply.github.com>
2025-05-27 10:45:51 +0000

    Add prefecture field population using faker

--- index.js
+++ index.js
@@ -4,8 +4,10 @@
         const name = faker.name.findName();
         const email = faker.internet.email();
         const phone = faker.phone.phoneNumber();
-
+        const prefecture_text = faker.address.state();
+        
         $('#name').val(name);
+        $('#prefecture_text').val(prefecture_text);
     } else {
         console.error('Faker chưa load');
     }
